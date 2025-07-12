023eb010ad8f275331beb53a78fa3b68d5a6e2a1
carlbrizuela <carlbrizuela@CJ>
2025-07-12 14:26:39 +0800

    Add new fields in sign-up form

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  #runs method before creation of new record
+  before_create :set_defaults 
+
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+
+  private 
+
+  def set_defaults
+    self.is_admin = false
+    self.balance = 1000000
+    self.status = false
+  end
+end
---
+++ db/migrate/20250712053632_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration[7.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :is_admin, :boolean
+    add_column :users, :balance, :integer
+    add_column :users, :status, :boolean
+  end
+end
