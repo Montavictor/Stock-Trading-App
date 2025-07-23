8014b561e3c326931002c6f9cf0a445dee39faf5
Victor Monta <montavictor1@gmail.com>
2025-07-23 19:52:01 +0800

    Design Changes, Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   before_action :check_if_user
   
   def index
+    @transactions = Transaction.where(created_at: Date.today.all_day)
+    @total = User.sum(:balance)
+    @users = User.where(is_admin: false)
   end
 end
